41b0ad624f4283780b80cd81913d0b828c823c19
DarshiniLokesh <darshini.lokesh22@gmail.com>
2025-08-18 22:28:56 +0530

    feat:getbalance

--- src/core/blockchain.ts
+++ src/core/blockchain.ts
@@ -1,4 +1,3 @@
-// src/core/blockchain.ts
 import { Block } from "./block";
 import { Transaction } from "./transaction";
 
@@ -15,7 +14,7 @@ export class Blockchain {
     this.miningReward = 10;
   }
 
-  createGenesisBlock(): Block {
+  private createGenesisBlock(): Block {
     const genesisBlock = new Block(0, [], "0");
     genesisBlock.hash = genesisBlock.calculateHash();
     return genesisBlock;
@@ -34,8 +33,11 @@ export class Blockchain {
       throw new Error("Transaction amount must be greater than zero");
     }
 
-   
-    if (transaction.fromAddress !== null && this.getBalanceOfAddress(transaction.fromAddress) < transaction.amount) {
+    // Check balance if not a reward transaction
+    if (
+      transaction.fromAddress !== null &&
+      this.getBalanceOfAddress(transaction.fromAddress) < transaction.amount
+    ) {
       throw new Error("Not enough balance");
     }
 
@@ -48,25 +50,22 @@ export class Blockchain {
       this.pendingTransactions,
       this.getLatestBlock().hash
     );
+
     block.mineBlock(this.difficulty);
     this.chain.push(block);
 
     // Reward miner
     this.pendingTransactions = [
-      new Transaction(null, minerAddress, this.miningReward)
+      new Transaction(null, minerAddress, this.miningReward),
     ];
   }
 
   getBalanceOfAddress(address: string): number {
     let balance = 0;
     for (const block of this.chain) {
-      for (const trans of block.transactions) {
-        if (trans.fromAddress === address) {
-          balance -= trans.amount;
-        }
-        if (trans.toAddress === address) {
-          balance += trans.amount;
-        }
+      for (const tx of block.transactions) {
+        if (tx.fromAddress === address) balance -= tx.amount;
+        if (tx.toAddress === address) balance += tx.amount;
       }
     }
     return balance;
@@ -76,7 +75,11 @@ export class Blockchain {
     if (previousBlock.index + 1 !== newBlock.index) return false;
     if (newBlock.previousHash !== previousBlock.hash) return false;
     if (newBlock.hash !== newBlock.calculateHash()) return false;
-    if (newBlock.hash.substring(0, this.difficulty) !== Array(this.difficulty + 1).join("0")) return false;
+    if (
+      newBlock.hash.substring(0, this.difficulty) !==
+      Array(this.difficulty + 1).join("0")
+    )
+      return false;
     return true;
   }
 
@@ -84,7 +87,10 @@ export class Blockchain {
     for (let i = 1; i < this.chain.length; i++) {
       const currentBlock = this.chain[i];
       const previousBlock = this.chain[i - 1];
-      if (!this.isValidNewBlock(currentBlock, previousBlock)) return false;
+
+      if (!this.isValidNewBlock(currentBlock, previousBlock)) {
+        return false;
+      }
     }
     return true;
   }
